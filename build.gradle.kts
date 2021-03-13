2320311fbb7836a8cd87e67e65a75b9b2713f012
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-03-13 17:37:11 +0900

    test: add test library

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val jda_version: String by project
 val lavaplayer_version: String by project
+val junit_version: String by project
 
 plugins {
     application
@@ -31,7 +32,11 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("net.dv8tion:JDA:$jda_version")
     implementation("com.sedmelluq:lavaplayer:$lavaplayer_version")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junit_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit_version")
 }
 
 tasks {
@@ -48,5 +53,8 @@ tasks {
         from(configurations.compileClasspath.get().filter { !it.name.endsWith("pom") }.map { if (it.isDirectory) it else zipTree(it) })
 
         exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    }
+    test {
+        useJUnitPlatform()
     }
 }
